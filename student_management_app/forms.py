000cae2d6a3069f8513de712b45b8a65c73b86d3
Pawan kumar <pawankr16123114@gmail.com>
2024-11-09 13:05:37 +0530

    Update forms.py

--- student_management_app/forms.py
+++ student_management_app/forms.py
@@ -1,11 +1,6 @@
 from django import forms 
 from django.forms import Form
 from student_management_app.models import Courses, SessionYearModel
-<form method="post">
-    {% csrf_token %}
-    <!-- form fields here -->
-    <button type="submit">Submit</button>
-</form>
 
 
 
