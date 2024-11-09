972037c7ed2d86b2f67f048ec591fbe2fabcdb6a
Pawan kumar <pawankr16123114@gmail.com>
2024-11-09 13:00:33 +0530

    Update forms.py

--- student_management_app/forms.py
+++ student_management_app/forms.py
@@ -1,6 +1,12 @@
 from django import forms 
 from django.forms import Form
 from student_management_app.models import Courses, SessionYearModel
+<form method="post">
+    {% csrf_token %}
+    <!-- form fields here -->
+    <button type="submit">Submit</button>
+</form>
+
 
 
 class DateInput(forms.DateInput):
